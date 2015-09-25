033b35b1b5dd2e692f8ce9b8d65ff7d167f2a452
Evan Light <evan@tripledogdare.net>
2015-09-25 17:33:52 -0400

    Removed (ineffectual) debugging

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,5 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-	console.log(request);
-	console.log(sender);
-	console.log(sendResponse);
 	if(request.url && request.filename) {
 	    chrome.downloads.download({
 		url: request.url,
