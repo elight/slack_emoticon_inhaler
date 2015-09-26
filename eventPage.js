125694cd84053241cd4cf08d7c45f347f490533c
caseywatts <casey.s.watts@gmail.com>
2015-09-25 20:42:37 -0400

    whitespace

--- eventPage.js
+++ eventPage.js
@@ -1,10 +1,11 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-	if(request.url && request.filename) {
-	    chrome.downloads.download({
-		url: request.url,
-		filename: request.filename
-	    });
-	}
+  function(request, sender, sendResponse) {
+    console.log(request.url + "-" + request.filename);
+    if(request.url && request.filename) {
+      chrome.downloads.download({
+        url: request.url,
+        filename: request.filename
+      });
     }
+  }
 );
